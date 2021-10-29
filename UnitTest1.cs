73dd979b60296aa6d01a9b028db5e3dc4347a740
David Gardiner <david@gardiner.net.au>
2021-10-29 21:14:32 +1030

    more

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace copy_config
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper _output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        
         [Fact]
         public void FileExists()
         {
+            var files = Directory.GetFiles(".");
+
+            foreach (var file in files)
+            {
+                _output.WriteLine(file);
+            }
+
             Assert.True(File.Exists("local.settings.json"));
         }
     }
